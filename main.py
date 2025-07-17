b33e0e6c8a721617103f93ee7cef29adf17b4b9d
IsaacCodes <179248168+IsaacCodes@users.noreply.github.com>
2025-07-16 17:47:29 -0700

    Improved color and monotone print speeds. Color is actually really fast now due to precalculating it in an nparray and printing a joined string. Also improved loading bar. Need to combine gbr and char frames inside of get_frames to improve .npy load times and other issues

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 from utils import scr, debug
 import processor
@@ -12,39 +13,52 @@ def main():
   print()
 
   target_fps = 10
-  start_time = time.time()
+  process_start_time = time.time()
 
   #Processes the image into frames
   with scr.term.cbreak(), scr.term.hidden_cursor():
-    frames = processor.get_frames(src_path, save_path, target_fps)
+    frames_arr = processor.get_frames(src_path, save_path, target_fps)
+    #Turns the [[color, char], [color, char], ...] frames_arr into [str, str, ...] frames
+    if not is_colored:
+      frames = [frame[1] for frame in frames_arr]
+    else:
+      frames = []
+      for bgr_frame, char_frame in frames_arr:
+        #Turns g, b, r int values into one ansi code str
+        #Signature tells it to operate on the groups of 3 (1d subarrays) instead of on the individual ints
+        color_vec = np.vectorize((lambda bgr: scr.term.color_rgb(bgr[2], bgr[1], bgr[0])), signature="(3)->()", otypes=[object])
+        #Combines the colors with their respective chars
+        colored_char_frame = color_vec(bgr_frame) + char_frame
+        frames.append(colored_char_frame)
 
   #Compilation info
-  print(f"{len(frames)} frame(s) compiled in {time.time() - start_time} s")
+  print(f"{len(frames)} frame(s) compiled in {time.time() - process_start_time} s")
   scr.wait_for_enter()
   scr.clear()
 
+  debug_times = []
   debug.print(f"Screen Size (H,W): ({scr.term.height}, {scr.term.width})")
+  debug.print(f"Frame Size (H,W): {frames[0][1].shape}")
 
   #Hides the cursor
   with scr.term.cbreak(), scr.term.hidden_cursor():
     #Loops through frames and displays them
     for frame in frames:
+      frame_start_time = time.time()
       scr.home()
-      bgr_frame, char_frame = frame
 
-      debug.print(f"Frame Size (H,W): {char_frame.shape}")
+      print("\n".join("".join(row) for row in frame))
 
-      for y in range(len(char_frame)):
-        for x in range(len(char_frame[0])):
-          if is_colored:
-            b, g, r = bgr_frame[y, x]
-            print(scr.term.color_rgb(r, g, b), end="")
+      delta_time = time.time() - frame_start_time
+      sleep_time = max(0, 1/target_fps - delta_time)
 
-          print(char_frame[y, x], end="")
-        print()
+      debug_times.append((time.time() - frame_start_time) * 1000)
+      debug.print(f"Time to print frame {debug_times[-1]} ms")
 
-      time.sleep(1/target_fps)
+      time.sleep(sleep_time)
 
+  debug.print(f"Total Time to print frame {sum(debug_times)} ms")
+  debug.print(f"AVG Time to print frame {sum(debug_times)/len(debug_times)} ms")
   print(scr.term.normal, end="")
   scr.wait_for_enter()
 
@@ -60,7 +74,4 @@ debug.print("Program stopped")
 # See if there are any other general performance optimizations
 # Account for printing time in time.sleep, could help smooth color performance
 # Would like to supress key codes in input() for scrolling + arrow keys
-# Could spice up loading bar. Ex:
-# print('Progress: [=======>   ]')
-# print(term.bold("60%"))
 # Better screen size management
--- utils.py
+++ utils.py
@@ -29,11 +29,12 @@ class Screen():
       print("[ Press Enter to Continue ]")
       while self.term.inkey() not in (self.term.KEY_ENTER, "\n", "\r"): pass
 
-  def progress_bar(self, completion: float, length=20):
+  def progress_bar(self, completion: float, length=25):
     hashtags = round(completion * length)
     spaces = length - hashtags
     
-    print("\r[ " + hashtags*"#" + spaces*" " + " ]", end="")
+    print("\r" + self.term.bold(f"{int(100*completion):>3}%"), end=" ")
+    print("[" + hashtags*"#" + spaces*" " + "]", end="")
     if completion == 1: 
       print("\nDone!\n")
 
